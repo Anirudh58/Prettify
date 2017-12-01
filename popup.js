04dd21d1bf3ed3668eaa7c3a53ebc61101c542ec
Anirudh58 <anirudh.jan97@gmail.com>
2017-12-01 11:43:00 +0530

    Keywords obtained through user input

--- popup.js
+++ popup.js
@@ -1,30 +1,33 @@
 
   document.addEventListener('DOMContentLoaded', function() {
     var hideButton = document.getElementById('hide');
-    //var keywords = document.getElementById('keywords').value;
     hideButton.addEventListener('click', function() {
 
-    function modifyDOM() {
+    var keywords = document.getElementById('keywords').value.split(",");
+    //console.log('key', keywords);
+
+    function modifyDOM(keywords) {
           //You can play with your DOM here or check URL against your regex
           //console.log('Tab script:');
 
           var posts = document.getElementsByTagName("p");
           console.log(posts);
+
+          keywords = keywords.split(",");
+          console.log(keywords);
           
           for(var i=0;i<posts.length;i++)
           {
             var string = posts[i].innerText.toLowerCase();
             //console.log(string);
 
-            var keywords = ["tag", "that friend", "that one friend"];
+            //var keywords = ["tag", "that friend", "that one friend"];
 
             var flag=0;
 
-            //console.log(keywords);
-
             for(var j=0;j<keywords.length;j++)
             {
-              if(string.indexOf(keywords[j].toLowerCase())!==-1){
+              if(string.indexOf(keywords[j])!==-1){
                 flag=1;
               }
             }
@@ -33,7 +36,7 @@
             {
               posts[i].parentElement.style.display = 'none';
               posts[i].parentElement.parentElement.style.display = 'none';
-              posts[i].parentElement.parentElement.parentElement.style.display = 'none';
+              //posts[i].parentElement.parentElement.parentElement.style.display = 'none';
             }
           }
 
@@ -42,10 +45,11 @@
 
     //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
     chrome.tabs.executeScript({
-        code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
+        code: '(' + modifyDOM + '("' + keywords + '"));' //argument here is a string but function.toString() returns function's code
     }, (results) => {
        //Here we have just the innerHTML and not DOM structure
        console.log('Popup script:')
+       //console.log(keywords);
 
        //console.log(results);
     });
